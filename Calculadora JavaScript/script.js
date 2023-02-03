bf5aacac7ab781cfe45a1e340daa35682e2afc37
LeandroGC15 <109327808+LeandroGC15@users.noreply.github.com>
2023-02-03 16:19:17 -0400

    Add files via upload

---
+++ Calculadora JavaScript/script.js
@@ -0,0 +1,35 @@
+//console.log("LEANDRO ESTO VA A SERVIR QUEDATE QUIETO");
+let num1= 13
+let num2= 12
+console.log("[1]suma\n" + "[2]resta\n" + "[3]multilicacion\n" + "[4] Division\n" + "[5] salir ")
+opc= 1
+switch(opc){
+    case 1:
+    if(num2 != 0)    {
+    R= nume1 + num2;
+        console.log("Suma" + R)}
+        else{
+console.log("Undefined")
+        }
+        break;
+        case 2:
+            R= num1 - num2;
+            console.log("resta" + R)
+            break;
+            case 3:
+                R= num1 * num2;
+                console.log("multilicacion" + R)
+                break;
+                case 4:
+               
+                R= num1 / num2
+                    console.log("division" + R)
+                    break;
+            
+                case 5:
+                    break;
+                    default:
+                        console.log("el valor se llama jose")
+                        break;
+
+}
